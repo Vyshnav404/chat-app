5f4c53dcff976efc21a09a5510f28c1caaec9edf
Vyshnav k <vyshnav.k@seqato.com>
2024-05-16 13:58:09 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const cookieparser = require('cookie-parser');
 
@@ -12,16 +13,19 @@ const { app,server } = require('./socket/socket');
 const port = process.env.PORT || 5000;
 
 dotenv.config();
+// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieparser());
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
-// app.get('/',(req,res)=>{
-//     //root route http://localhost:5000/
-//     res.send("Hello World !!!");
-// })
+
+app.use(express.static(path.join(__dirname,'..', "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname,'..', "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () =>{
 connectDB();    
